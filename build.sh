e7bbe610792112c7889f68e2f4699bec7ea633bc
Frank Wei <fwei1729@gmail.com>
2026-04-25 17:25:16 -0400

    Added R2 references

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -e
 
 QUARTO_VERSION=1.9.37
 QUARTO_DIR=/tmp/quarto-${QUARTO_VERSION}
+FILES_BASE_URL=https://pub-f7acf1ba602e433f9a8bd0e03679fb8c.r2.dev
 
 # Install quarto
 wget -q https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz -O /tmp/quarto.tar.gz
@@ -10,4 +11,7 @@ tar -xzf /tmp/quarto.tar.gz -C /tmp
 
 # Run render command in Quarto folder
 export PATH=${QUARTO_DIR}/bin:$PATH
+
+find . -name "*.qmd" -type f -exec sed -i "s|@FILES_BASE_URL@|${FILES_BASE_URL}|g" {} +
+
 quarto render .
